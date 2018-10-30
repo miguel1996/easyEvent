7c9cf2468885466846f55c6814866dfa56cc00b3
toomaas <toomaas8@hotmail.com>
2018-10-30 14:03:35 +0000

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call('UserTableSeeder');
         // $this->call(UsersTableSeeder::class);
+    }
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        DB::table('users')->insert([
+            'name' => 'tomas',//str_random(10),
+            'email' => 'tomas@hotmail.com',//str_random(10).'@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
     }
 }
