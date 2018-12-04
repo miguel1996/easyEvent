3b264c024e4175cb5a1d6625cbce1bb727276967
toomaas <toomaas8@hotmail.com>
2018-12-04 11:38:49 +0000

    model many to many relationsship

---
+++ app/Element.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Element extends Model
+{
+    public function events(){
+        return $this->belongsToMany('App\Event','events_elements');
+    }
+}
