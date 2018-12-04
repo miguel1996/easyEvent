d91dfb5147128d71107dbbfad072b447ad744a0c
toomaas <toomaas8@hotmail.com>
2018-12-04 09:59:38 +0000

    create groups table and update users table

---
+++ database/migrations/2018_12_04_094746_update_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('date_of_birth');
+            $table->string('address');
+            $table->string('phone_number')->nullable();
+            $table->enum('gender',['male','female','i prefer not to say','all of the above']);
+            $table->integer('group_id')->unsigned()->index();
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
