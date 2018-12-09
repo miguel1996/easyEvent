e98c4e8a721ee06fc9a8158cf6f6684fd436d786
toomaas <toomaas8@hotmail.com>
2018-12-09 23:29:42 +0000

    added checkbox with enums options

--- public/js/scripts/events.js
+++ public/js/scripts/events.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     $("#addElement").click(function(){
         numElements++;
         $("#fields_zone").append('<li>Extra field '+numElements+': <input type="text" name="label'+numElements+'">');
-        $("#fields_zone").append('<select>'+$(".enums").each(function(){
-            $("#fields_zone").append('<option value='+$(this).attr('value')+'>'+$(this).attr('value')+'</option>');
-        })+'</select></li>');
+        $("#fields_zone").append('<select id=enumSelect></select></li>');
+        $(".enums").each(function(){
+            $("#enumSelect").append('<option value='+$(this).attr('value')+'>'+$(this).attr('value')+'</option>');
+        });
     });
-    
 });
 
