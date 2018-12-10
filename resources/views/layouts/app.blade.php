b7bfbea8cadfc7e6b578d735c61a32b9d438f94c
Miguel <miguel.alex.andrade@gmail.com>
2018-12-10 14:55:42 +0000

    Layout
    Overall changes to the layout file/style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
