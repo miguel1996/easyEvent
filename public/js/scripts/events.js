1f801261065bbb3cd2cf31ce13b40d0663859b90
mlfranco-pt <31574205+mlfranco-pt@users.noreply.github.com>
2018-12-13 15:44:47 +0000

    form field introduction condition, only if both is filled

--- public/js/scripts/events.js
+++ public/js/scripts/events.js
@@ -1,18 +1,27 @@
 var numElements = 0;
 $(document).ready(function(){
-    $("#addElement").click(function(){
-        numElements++;
-       //will append a list of the possible enums and the necessary fields to insert an extra element to the event subscription
-        $("#fields_zone").append('<li>Extra field '+numElements+': <input type="text" name="label'+numElements+'">');
-        $("#fields_zone").append('<select id="enumSelect'+numElements+'" name="enumSelect'+numElements+'"></select></li>');
-        $(".enums").each(function(){
-            $("#enumSelect"+numElements).append('<option value='+$(this).attr('value')+'>'+$(this).attr('value')+'</option>');
-        // the page will scroll to the button so that when we add an element we can see the part of the page that was updated
-            $([document.documentElement, document.body]).animate({
-            scrollTop: $("#addElement").offset().top
-        }, 1); //this blocked with 2000 instead of 1
-        $("#numOfElements").val(numElements);
-        });
+    $("#addElement").click(function(){      
+            
+        if ($("#field"+numElements).val() != "" && $("#enumSelect" + numElements).val() != "") {
+            
+            console.log(numElements);
+            numElements++;
+
+            //will append a list of the possible enums and the necessary fields to insert an extra element to the event subscription
+            $("#fields_zone").append("<li>Extra field " + numElements + ': <input id="field' + numElements + '" type="text" name="label' + numElements + '" required>');
+            $("#fields_zone").append('<select id="enumSelect' + numElements + '" name="enumSelect' + numElements + '" required></select></li>');
+            $(".enums").each(function() {
+                $("#enumSelect" + numElements).append('<option style="display:none">' + "</option>");
+                $("#enumSelect" + numElements).append("<option value=" + $(this).attr("value") + ">" + $(this).attr("value") + "</option>");
+                // the page will scroll to the button so that when we add an element we can see the part of the page that was updated
+                $([
+                    document.documentElement,
+                    document.body
+                ]).animate({ scrollTop: $("#addElement").offset().top }, 1); //this blocked with 2000 instead of 1
+                $("#numOfElements").val(numElements);
+            });
+        };
     });
+
 });
 
