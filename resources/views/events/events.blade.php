96c0a2319f098759ded266f7b5f832b1630bdddc
Miguel <miguel.alex.andrade@gmail.com>
2018-12-13 17:29:36 +0000

    Further Interface Changes
    Changed background
    Changed Navbar CSS (hover and selected page)
    New redirect after login

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -4,6 +4,9 @@
 @stop
 
 @section('content')
+<script type= "text/javascript">
+    $(document).ready(function(){$("#all-events-button").addClass("active");});
+</script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
