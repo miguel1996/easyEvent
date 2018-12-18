84f43e8c7a7d2534263115efa5b5d540ae690d29
toomaas <toomaas8@hotmail.com>
2018-12-18 15:50:27 +0000

    welcome controller only shows events that are open to subscribe v1

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Event;
+use Carbon\Carbon;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
         // $event = Event::find($id);
-        $events = Event::all();
+        $events = Event::whereDate('opening_subscription_date','<', Carbon::now()->toDateTimeString())->get();
         return view('welcome',compact('events'));
     }
 }
