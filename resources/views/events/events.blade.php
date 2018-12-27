5fe9ca15b3eb043e50efb89faa87b053c52548df
toomaas <toomaas8@hotmail.com>
2018-12-27 16:29:06 +0000

    updated all events page to show more about the events

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -11,13 +11,21 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"><h4>All events</h4></div>
+                @foreach($events as $event)               
                     <div class="card-body">
                         <ul>
-                            @foreach($events as $event)
-                                <li> <a href="/events/{{$event->id}}">{{$event->title}}</a> </li>
-                            @endforeach
+                            <h4><li> {{$event->title}} </li></h4>
+                            <li><img src="/images/event_photos/{{$event->image_path}}" heigth="100" width=100></li>
+                            <h5>
+                            <li>{{substr($event->description,0,150)}}...</li>
+                            <li>open from: {{$event->opening_subscription_date}} to: {{$event->closing_subscription_date}}</li>
+                            <li>Event date: {{$event->event_date}}</li>
+                            <li><a href="/events/{{$event->id}}">More</a></li>
+                            </h5>                   
                         </ul>
-                    </div>
+                        
+                    </div>                
+                    @endforeach
                 </div>
             </div>
         </div>
