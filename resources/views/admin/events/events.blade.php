02d69ac94edfda8b4ee3d9cdf3a68e86bb11062b
toomaas <toomaas8@hotmail.com>
2018-12-29 14:49:35 +0000

    added small info about event state in admin/events

--- resources/views/admin/events/events.blade.php
+++ resources/views/admin/events/events.blade.php
@@ -14,12 +14,26 @@
 	<div class="card-body">
 	<table>
 	<tr>
-	<th>Event</th><th>Manager</th><th>Event date</th><th>Actions</th>
+	<th>Event</th><th>Manager</th><th>Event date</th><th>Info</th><th>Actions</th>
 	</tr>
 	@foreach($events as $event)
     @php($user = \App\User::find($event->user_id))
         <tr> 
-		<td>{{$event->title}} </td><td>{{$user->name}}</td><td>{{$event->event_date}}</td><td><a href="/user/events/{{$event->id}}">Manage</a></td>
+			<td>{{$event->title}} </td>
+			<td>{{$user->name}}</td>
+			<td>{{$event->event_date}}</td>
+			<td>
+				@if($event->opening_subscription_date > \Carbon\Carbon::now())
+				Subscriptions have not been opened
+				@elseif($event->opening_subscription_date < \Carbon\Carbon::now() && $event->closing_subscription_date > \Carbon\Carbon::now())
+				Open for subscriptions
+				@elseif($event->closing_subscription_date < \Carbon\Carbon::now() && $event->event_date > \Carbon\Carbon::now())
+				Subscriptions closed
+				@elseif($event->event_date < \Carbon\Carbon::now())
+				Event Completed
+				@endif
+			</td>
+			<td><a href="/user/events/{{$event->id}}">Manage</a></td>
 		</tr>
     @endforeach
 	</table>
